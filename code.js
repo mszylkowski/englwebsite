befebae361a776d2fd53615217c57055e294f6a9
Matias Szylkowski <mszylkowski@hotmail.com>
2017-04-04 16:26:06 -0400

    ore changes

--- code.js
+++ code.js
@@ -9,3 +9,11 @@ $(window).load(function () {
 function show_login() {
     window.open("//login.html","_self");
 }
+
+function login() {
+    if ($("#username").val() == "crake") {
+        window.open("users/crake/dashboard.html");
+    } else if ($("#username").val() == "jimmy") {
+        window.open("users/jimmy/dashboard.html");
+    }
+}
